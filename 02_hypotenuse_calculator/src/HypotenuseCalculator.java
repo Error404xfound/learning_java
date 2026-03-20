6e8d46bc09c208da382c722fd07cdbfada8cbde2
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-03-20 22:05:26 +0800

    refactor: rename num1 & num2 to side 1 & side2

--- 02_hypotenuse_calculator/src/HypotenuseCalculator.java
+++ 02_hypotenuse_calculator/src/HypotenuseCalculator.java
@@ -4,19 +4,19 @@ public class HypotenuseCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double num1;
-        double num2;
+        double side1;
+        double side2;
         double hypotenuse;
 
         System.out.println("---Albus' Hypotenuse Calculator---");
 
-        System.out.print("Enter number 1 (cm): ");
-        num1 = scanner.nextDouble();
+        System.out.print("Enter side 1 (cm): ");
+        side1 = scanner.nextDouble();
 
-        System.out.print("Enter number 2 (cm): ");
-        num2 = scanner.nextDouble();
+        System.out.print("Enter side 2 (cm): ");
+        side2 = scanner.nextDouble();
 
-        hypotenuse = Math.sqrt(Math.pow(num1,2) + Math.pow(num2,2));
+        hypotenuse = Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2));
         System.out.println("\nThe hypotenuse is " + String.format("%.2f", hypotenuse) + " cm");
 
         scanner.close();
