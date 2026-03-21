91b1c0ff31476f9ee2a8ab62ad5985b440e3f4c8
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-03-22 01:03:32 +0800

    feat: add weight converter project

---
+++ 04_weight_converter/src/WeightConverter.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class WeightConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String option;
+        double amount;
+
+        System.out.println("\n=====Weight Converter Program=====");
+        System.out.println("1. Convert pound to kilograms");
+        System.out.println("2. Convert kilograms to pound");
+        System.out.println("3. Exit program\n");
+        System.out.println("----------------------------------");
+        System.out.print("Enter your selection (1/2/3): ");
+        option = scanner.nextLine();
+
+        if (option.equals("3")) {
+            System.out.println("\nExiting Program...");
+            System.out.println("Goodbye!");
+            System.exit(0);
+        }
+
+        System.out.println("\nWhat is the amount of weight you want to convert?");
+        if (option.equals("1")) {
+            System.out.print("Enter amount (pounds): ");
+            amount = scanner.nextDouble();
+            System.out.printf("\n%.2f pounds is equal to %.2f kilograms", amount, amount * 0.45359237);
+        }
+        else if (option.equals("2")) {
+            System.out.print("Enter amount (kilograms): ");
+            amount = scanner.nextDouble();
+            System.out.printf("\n%.2f kilograms is equal to %.2f pounds", amount, amount * 2.2046226218);
+        }
+        else {
+            System.out.println("Invalid Input");
+        }
+
+        scanner.close();
+    }
+}
